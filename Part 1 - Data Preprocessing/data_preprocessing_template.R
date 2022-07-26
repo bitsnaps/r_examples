95b7de57039d60bd0f72500db08246d7b5bf7404
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-26 12:54:40 +0000

    split data into training and test sets

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,28 +1,7 @@
 # Data Preprocessing Template
 
-# Set current working directory
-#setwd("/cloud/project/Part 1 - Data Preprocessing")
-
 # Importing the dataset
 dataset = read.csv('Data.csv')
-
-# Dealing with missing data
-dataset$Age = ifelse(is.na(dataset$Age), 
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary = ifelse(is.na(dataset$Salary), 
-                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary)
-# Encoding categorical data
-dataset$Country = factor(dataset$Country,
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased,
-                           levels = c('No', 'Yes'),
-                           labels = c(0, 1))
-
-
-
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
