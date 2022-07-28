1059c3b3578c5d8cfdb6d5817042803c011a2797
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-28 13:22:38 +0000

    fix: dataset path

--- Part 3 - Classification/3_svm/svm.R
+++ Part 3 - Classification/3_svm/svm.R
@@ -1,7 +1,7 @@
 # Support Vector Machine (SVM)
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
+dataset = read.csv('../Social_Network_Ads.csv')
 dataset = dataset[3:5]
 
 # Encoding the target feature as factor
