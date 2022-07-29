8d1542dfa4a20d9ea915d8e0acc076b7fc5a2f33
Ibrahim.H <bitsnaps@yahoo.fr>
2022-07-29 23:16:33 +0100

    add eclat algorithm example

---
+++ Part 5 - Association Rule/2_eclat/eclat.R
@@ -0,0 +1,19 @@
+# Eclat
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+data_file = '../Market_Basket_Optimisation.csv'
+dataset = read.csv(data_file)
+# Convert dataset to a Sparse Matrix & remove duplicates
+dataset = read.transactions(data_file, sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+# use a function to plot item frequency from arules package
+itemFrequencyPlot(dataset, topN = 10)
+
+# Training Eclat on the dataset
+rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
+# There is no confidence value for Eclat
+
+# Visualising the results sorted by support level
+inspect(sort(rules, by = 'support')[1:10])
